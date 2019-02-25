dfbb18eb8149bcff6069c03c46044eceb3e83df2
yichunzhao <zhaoyichun@hotmail.com>
2019-02-25 23:55:54 +0100

    future, executor-service and executors

---
+++ src/main/java/ThreadPool/ExecutorServiceSingleThread.java
@@ -0,0 +1,31 @@
+package ThreadPool;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @author YNZ
+ */
+public class ExecutorServiceSingleThread {
+
+    public static void main(String... args) throws ExecutionException, InterruptedException {
+        //Create a single thread worker and an unbound queue.
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> System.out.println("Hello World"));
+
+        //ScheduledExecutorService -> ExecutorService -> Executor.
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> System.out.println("Hello XXX"));
+
+        //Submit Asynchronous tasks(callable), to the queue.
+        //If you don’t want your Callable to return a value, you can create
+        //it using Callable<Void>.
+        Future<String> future = executorService.submit(()-> "callable task");
+
+        String result = future.get();
+        System.out.println("Result: "+ result);
+    }
+}
