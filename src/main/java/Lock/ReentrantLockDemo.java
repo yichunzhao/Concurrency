186c0ed9816aa2e52b8fcb9e6c3362e6890470ac
yichunzhao <zhaoyichun@hotmail.com>
2019-05-18 03:09:46 +0200

    ReentrantLock demo

---
+++ src/main/java/Lock/ReentrantLockDemo.java
@@ -0,0 +1,48 @@
+package Lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockDemo {
+    //a mutex, avoid deadlock.
+    private static ReentrantLock lock = new ReentrantLock();
+
+
+    public static void main(String[] args)  {
+
+        Thread th1 = new Thread(() -> accessResource());
+        Thread th2 = new Thread(() -> accessResource());
+        Thread th3 = new Thread(() -> accessResource());
+        Thread th4 = new Thread(() -> accessResource());
+
+        th1.start();
+        th2.start();
+        th3.start();
+        th4.start();
+    }
+
+    //replacing the synchronise
+    public static void accessResource()  {
+        //lock the resource, only one thread may access.
+        boolean lockAcquired = false;
+        try {
+            lockAcquired = lock.tryLock(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //access the resource here
+
+        if (lockAcquired) {
+            try {
+                //access resource
+
+            } finally {
+                lock.unlock();
+            }
+
+        } else {
+
+        }
+    }
+}
