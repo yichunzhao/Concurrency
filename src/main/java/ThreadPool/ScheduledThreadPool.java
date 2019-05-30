2b53cbab2ed124d04b06fab950ccefd2a84b61f0
yichunzhao <zhaoyichun@hotmail.com>
2019-05-30 18:53:19 +0200

    updating schedule thread pool.

--- src/main/java/ThreadPool/ScheduledThreadPool.java
+++ src/main/java/ThreadPool/ScheduledThreadPool.java
@@ -1,9 +1,7 @@
 package ThreadPool;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,25 +14,34 @@ class MonitoringTask implements Runnable {
     public void run() {
 
         System.out.println("do monitoring on the target ...");
+
         try {
-            Thread.sleep(100);
+            Thread.sleep(10);
+            System.out.println(Thread.currentThread().getName() + " running ...");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println();
     }
 }
 
 public class ScheduledThreadPool {
 
     public static void main(String[] args) throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(3);
 
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
-        ScheduledFuture<?> future = executor.scheduleAtFixedRate(new MonitoringTask(), 1, 5, TimeUnit.SECONDS);
 
-        countDownLatch.await(6, TimeUnit.SECONDS);
-        future.cancel(true);
+        //schedule a task to run after 10 sec.
+        executor.schedule(MonitoringTask::new, 5, TimeUnit.SECONDS);
+
+        //schedule a task  to run periodically every 16 sec.
+        executor.scheduleAtFixedRate(new MonitoringTask(), 1, 10, TimeUnit.SECONDS);
+
+        //schedule to repeat a task periodically, with a 15 delay as the pre-task completes
+        executor.scheduleWithFixedDelay(MonitoringTask::new, 5, 10, TimeUnit.SECONDS);
+
+
+        executor.awaitTermination(160, TimeUnit.SECONDS);
+        executor.shutdown();
 
     }
 
