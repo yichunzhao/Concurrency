2b1454fc4060f1893fe74297dcd453eff64bc50d
yichunzhao <zhaoyichun@hotmail.com>
2020-10-31 12:01:48 +0100

    propose a task about using Semaphore and blocking Q

---
+++ src/main/java/ProducerConsumer/Semaphore/ProducerConsumerSemaphoreDemo.java
@@ -0,0 +1,18 @@
+package ProducerConsumer.Semaphore;
+
+/**
+ * Using Semaphore to implement a blocking-queue, and demo a producer and consumer pattern.
+ * <p>
+ * A blocking queue allows one thread to putting int it, while another one thread to taking from it.
+ * <p>
+ * When the queue is empty, the thread taking from it is blocked; and when the queue is full, the thread putting into
+ * it is blocked.
+ */
+
+
+public class ProducerConsumerSemaphoreDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
